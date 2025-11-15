ab2441653f807452bc44873a1b46cc2873e4dca1
Maayank18 <maayank51518@gmail.com>
2025-11-15 17:54:17 +0530

    added authentication to routes in frontend for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import { useQuery } from '@tanstack/react-query'
 
 const App = () => {
   return (
-    <div className='bg-red-500 h-screen'>
+    <div className='bg-red-500 h-screen' data-theme="night">
       <button className="btn">Button</button>
       <button className="btn btn-neutral">Neutral</button>
       <button className="btn btn-primary">Primary</button>
