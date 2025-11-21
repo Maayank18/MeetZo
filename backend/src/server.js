5c577991650fdcffa0563e57dcb79261883b6e65
Maayank18 <maayank51518@gmail.com>
2025-11-21 16:07:55 +0530

    added commits added changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,51 +1,9 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import authRoutes from "./routes/auth.js";
-// import userRoutes from "./routes/user.js";
-// import chatRoutes from "./routes/chat.js";
-// import { connectDB } from "./lib/db.js";
-
-// // Load environment variables FIRST
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5001;
-
-// // ---- Middlewares ----
-// app.use(express.json());
-// app.use(cookieParser());
-
-// // CORS (MUST include credentials for cookies to work)
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-
-// // ---- Routes ----
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/chat", chatRoutes);
-
-// // ---- Start Server Only After DB Connect ----
-// connectDB()
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log(`Server running on port ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("DB connection failed:", err);
-//   });
-
 import express from "express";
-import "dotenv/config";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+dotenv.config();
 
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
@@ -76,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
   // Since we run this from inside 'backend', '..' goes up to root, then into 'frontend'
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
